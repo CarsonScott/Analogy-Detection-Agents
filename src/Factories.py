ca53c11727e7851588e4be3d831cd3bc9876e32d
Carson Scott <carsonjscott14@gmail.com>
2017-09-14 16:34:22 -0400

    Added competitive learning

---
+++ src/Factories.py
@@ -0,0 +1,26 @@
+from Detector import Detector
+from Network import Network
+from Layer import Layer
+from random import randrange as rr
+
+def RandomDetector(inputs, nodes, lower=0, upper=0):
+	d = Detector(inputs, nodes)
+	for i in range(nodes):
+		for j in range(inputs):
+			d.biases[i][j] = rr(lower, upper)/100
+	return d
+
+def RandomNetwork(inputs, layers, sizes):
+	n = Network(inputs, layers, sizes)
+	sizes = [inputs]+sizes
+	for i in range(layers):
+		layer = RandomDetector(sizes[i], sizes[i+1])
+		n.layers[i] = layer
+	return n
+
+def RandomLayer(inputs, nodes, lower=0, upper=100):
+	d = Layer(inputs, nodes)
+	for i in range(nodes):
+		for j in range(inputs):
+			d.weights[i][j] = rr(lower, upper)/100
+	return d
---
+++ src/Layer.py
@@ -0,0 +1,45 @@
+e = 2.718281828459
+
+def logistic(x, a, b, c):
+	return a / (1+pow(e,-b*(x-c)))
+
+class Layer:
+
+	def __init__(self, inputs, nodes):
+		self.inputs = []
+		self.totals = []
+		self.outputs = []
+		self.weights = []
+		self.lrate = 0.0001
+		self.drate = 0.00001
+	
+		for i in range(nodes):
+			self.totals.append(0)
+			self.outputs.append(0)
+			self.weights.append([])
+			for j in range(inputs):
+				self.weights[i].append(1)
+
+	def compute(self, inputs):
+		self.inputs = inputs
+		for i in range(len(self.weights)):
+			x = 0
+			for j in range(len(self.weights[i])):
+				x += self.inputs[j] * self.weights[i][j]
+
+			self.totals[i] = x
+			self.outputs[i] = logistic(x, 1, 2, 0.5)
+
+		o = self.outputs
+		for i in range(len(self.outputs)):
+			y = 0
+			for j in range(len(o)):
+				if i != j: y += o[j] * self.lrate 
+			self.outputs[i] -= y
+		return self.outputs
+
+	def update(self):
+		for i in range(len(self.weights)):
+			for j in range(len(self.weights[i])):
+				dw = self.outputs[i]*self.inputs[i]*self.weights[i][j]/self.totals[i]
+				self.weights[i][j] + dw * self.drate
